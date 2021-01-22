bc4c3529f2b3244dcfec5a9ca804d4bbbdcc68da
Fan Wu <fanwu@pm.me>
2021-01-22 09:42:46 +0800

    支持多文件名

--- main.go
+++ main.go
@@ -11,14 +11,21 @@ func main() {
 	app := &cli.App{
 		Name:  "box_log_count",
 		Usage: "计数包厢房态",
+		Flags: []cli.Flag{
+			&cli.StringSliceFlag{
+				Name:    "logs",
+				Aliases: []string{"L"},
+				Usage:   "Load logs from `FILES`",
+			},
+		},
 		Action: func(c *cli.Context) error {
 			if c.Args().Len() < 1 {
 				fmt.Printf("请输入房态日志文件名来进行解析 \n")
 				_ = cli.Exit("必须提供日志文件路径", 1)
 			}
-			fileName := c.Args().Get(0)
-			fmt.Printf("解析日志文件： %v \n", fileName)
-			processLogFile(fileName)
+			logs := c.StringSlice("logs")
+			fmt.Printf("解析日志文件： %v \n", logs)
+			processLogFiles(logs)
 			return nil
 		},
 	}
@@ -29,10 +36,16 @@ func main() {
 	}
 }
 
-func processLogFile(logFile string) {
+func processLogFiles(logFiles []string) {
 	initData()
-	file, isGzip, _ := getFile(logFile)
-	defer file.Close()
-	_ = process(file, isGzip)
+	log.Printf("Start process %d log files: \n", len(logFiles))
+	for _, f := range logFiles {
+		log.Printf("process log file: %v \n", f)
+		file, isGzip, _ := getFile(f)
+		_ = process(file, isGzip)
+		_ = file.Close()
+		log.Printf("finish the log File \n")
+	}
 	outPut()
+	log.Printf("All Done \n")
 }
